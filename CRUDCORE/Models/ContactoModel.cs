a3125036412584de407341cec38ddf4176e41c93
Marlon Villalona <iamvillalona@gmail.com>
2022-04-16 15:38:37 -0400

    Crud .Net Core & SQL SERVER

---
+++ CRUDCORE/Models/ContactoModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDCORE.Models
+{
+    public class ContactoModel
+    {   
+        // Estas propiedades hacen referencia a cada una de las columnas de las tablas
+        public int IdContacto { get; set; }
+
+        [Required(ErrorMessage = "El campo nombre es obligatorio")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "El campo telefono es obligatorio")]
+        public string? Telefono { get; set; }
+        [Required(ErrorMessage = "El campo correo es obligatorio")]
+        public string? Correo { get; set; }
+
+    }
+}
